9a90e3aeb671426a442a7e7a4b0f50448797df5c
Vitaly <lub-olen@yandex.ru>
2021-12-06 17:19:03 +0300

    Initial Commit

---
+++ TheLightUI/TheLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUIApp.swift
+//  TheLightUI
+//
+//  Created by Vitally Ochnev on 06.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
